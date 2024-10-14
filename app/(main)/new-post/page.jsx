dca7f5569280f0c680e8c03b16726f29d996fa3c
Elif <elif.keskin233@gmail.com>
2024-10-14 16:41:45 +0300

    new post page.jsx has written temporary

---
+++ app/(main)/new-post/page.jsx
@@ -0,0 +1,14 @@
+export default function NewPost() {
+    return (
+        <div>
+            <form action="">
+
+                <input type="text" name="title" placeholder="yazı vaşlığı" required />
+                <br />
+                <input type="content" name="content" id="" placeholder="yazı içerği" required />
+                <br />
+                <button>yaızyı paylaş</button>
+            </form>
+        </div>
+    )
+}
